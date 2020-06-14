97c4c841073ce2db0e301c68ce1b1ab2d81720a1
M-shahzad-ali <64818830+M-shahzad-ali@users.noreply.github.com>
2020-06-14 17:11:37 +0500

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,29 @@
+//============q1
+//var a,b,c;
+
+
+
+
+//=============q2
+//legal variable name;
+/*var a;
+var $name;
+var _name;
+var varCheck;
+var boll_same;
+
+//illegeal variable;
+var 1a;
+var x ray;
+var @name;
+var 123;
+
+
+
+
+// ===========q3;
+
+"variable name can only contain ,number,$,and _;"
+variable name are case sensitive;
+variable name should not be js keyword*/
+
---
+++ app9.js
@@ -0,0 +1,145 @@
+//var arr=[[, ,],[, ,],[, ,]]
+
+
+
+/*var arr=[[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+
+for(var i=0;i<3;i++){
+    for(var j=0;j<4;j++){
+        document.write(arr[i][j]+"  ");
+    }
+
+    document.write("<br>");
+}
+
+
+for(var i=1;i<11;i++)
+    {
+
+
+        document.write(i+"<br>")
+    }
+
+
+
+    var a=+prompt("enter table number");
+    var b=+prompt("enter length of table");
+    document.write("multiplication  table  of "+a+"length"+b +"<br>");
+
+    for(var i=1;i<=b;i++){
+       
+
+        document.write(a+"*"+i+"="+a*i+"<br>");
+    }
+
+    var fruits=["apple","banana","mango","orange","strawberry"];
+
+    for(i=0;i<fruits.length;i++){
+
+        document.write(fruits[i]+" <br>")
+    }
+
+    document.write("<br> <br>")
+    for(i=0;i<fruits.length;i++){
+
+        document.write("element at index"+"  "+i+" "+"is"+"  "+ fruits[i]+"<br>");
+    }
+    
+    
+    var i;
+    document.write("counting"+"<br>");
+    for(i=1;i<=15;i++){
+
+        document.write(i+",")
+    }
+    document.write("<br><br><br>")
+
+    document.write("reverse counting"+"<br>");
+    for(i=10;i>0;i--){
+
+        document.write(i+",");
+    }
+    document.write("<br><br><br>")
+    document.write("even"+"<br>")
+    for(i=0;i<=20;i=i+2){
+
+        document.write(i+",")
+    }
+    document.write("<br><br><br>");
+    document.write("odd"+"<br>");
+
+    for(i=1;i<=20;i=i+2){
+        document.write(i+",")
+    }
+    document.write("<br><br><br>");
+    document.write("series"+"<br>");
+
+    for(i=2;i<=20;i=i+2){
+
+        document.write(i+"k"+",");
+    }
+
+
+
+
+
+
+
+var a=prompt("welcome to karachi bakery .what do you want to order sir");
+
+var arr=["cake","apple pie","cookie","chips","patties"]
+
+for(i=0;i<arr.length;i++){
+
+    if(arr[i]==a){
+
+        document.write(arr[i]+"is available at index"+i+"in our bakery");
+    }
+   else{
+
+    document.write("we are sorry"+" "+a+" "+"is not avaibale in our bakery");
+    break;
+   }
+}
+
+
+
+var arr=[24,53,78,91,12];
+
+document.write("array item:"+arr+"<br>");
+for(i=0;i<arr.length;i++){
+
+    if(arr[0]<arr[i]){
+        arr[0]=arr[i];
+        
+
+        
+    }
+}
+document.write("the largest number is"+arr[0])
+
+var arr=[24,53,78,91,12];
+
+document.write("array item:"+arr+"<br>");
+for(i=0;i<arr.length;i++){
+
+    if(arr[0]>arr[i]){
+        arr[0]=arr[i];
+        
+
+        
+    }
+}
+document.write("the smallest number is"+"  "+arr[0])
+
+
+
+
+
+for(i=1;i<=20;i++){
+
+    document.write(i*5+",");
+}*/
+
+
+
